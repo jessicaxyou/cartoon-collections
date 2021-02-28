f11f310cbe49134b1557e963d17a3ecc18631427
jessica you <jessicaxytan@gmail.com>
2021-02-28 13:43:17 -0500

    commit tree 8bad7111321bfe4ebb66849194733a8e0c9528d1

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,45 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+
+def roll_call_dwarves(names)
+
+  names.collect.with_index(1) do |name, index|
+    puts "#{index}.*#{name}/"
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(elements)
+  
+  new_elements = elements.map(&:capitalize) #updating array to capitalize each word
+
+  new_elements.map do |word|
+    word + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(items)
+
+  items.each.any? do |letters|
+    if letters.length > 4
+      true
+    else
+      false
+    end
+  end
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  foods.find { |cheese| cheese_types.include?(cheese)}
+
+
 end
